5381547a20f7eb929b8d9e2161532d72065850b3
Kristo Roots <31840227+rootkris@users.noreply.github.com>
2019-03-17 18:01:20 +0200

    Buttons
    Added functions to the buttons

--- app/src/main/java/com/example/rakendus/MainActivity.java
+++ app/src/main/java/com/example/rakendus/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.rakendus;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void toastMe(View view){
+        Toast myToast = Toast.makeText(this, "Hello Toast!",
+                Toast.LENGTH_SHORT);
+        myToast.show();
+    }
+
+    public void countMe(View view){
+        TextView showCountTextView = (TextView) findViewById(R.id.textView);
+        //Get the value of the text view
+        String countString = showCountTextView.getText().toString();
+        //Convert value to number and increment it
+        Integer count = Integer.parseInt(countString);
+        count++;
+        //Display the new value in the text view
+        showCountTextView.setText(count.toString());
+    }
+
+    public void randomMe(View view){
+        Intent randomIntent = new Intent(this, Random.class);
+        startActivity(randomIntent);
+    }
+
 }
